7c1c41e9add6afba69b01ebdef29e4bccafec929
naimulabeer <naimul.h.abeer@gmail.com>
2023-09-15 01:01:37 +0600

    project initialized with vite + react + tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl text-[#1C1B1B] font-bold text-center">
+        Course Registration
+      </h1>
+    </>
+  );
+}
+
+export default App;
